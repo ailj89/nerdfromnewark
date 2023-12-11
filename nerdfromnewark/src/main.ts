e1dbd94f1bb05de82816c7d14cea40ca72a75994
Andre Jordan <Dre@Dres-Macbook.local>
2023-12-11 12:09:02 -0500

    added vuetify

--- nerdfromnewark/src/main.ts
+++ nerdfromnewark/src/main.ts
@@ -3,6 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 import App from './App.vue'
 import router from './router'
 
@@ -11,4 +22,4 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.use(vuetify).mount('#app')
---
+++ nerdfromnewark/src/views/RolesView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="about">
+        <h1>This is my career page</h1>
+
+      <div class="careers">
+        <div class="career">
+            <v-card text="..."></v-card>
+
+            <v-list lines="one">
+                <v-list-item
+                    v-for="n in 3"
+                    :key="n"
+                    :title="'Item ' + n"
+                    subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+                ></v-list-item>
+            </v-list>
+        </div>
+      </div>
+    </div>
+</template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
